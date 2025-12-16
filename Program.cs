4f2228f634d4d5201df4588e8d7a1c489bc1d959
Swapnil Lakra <swapnil@192.168.1.26>
2025-12-16 17:52:27 +0530

    Employee Management System Console UI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("\n--- Employee Management System ---");
+            Console.WriteLine("1. Create Employee");
+            Console.WriteLine("2. Update Employee");
+            Console.WriteLine("3. Delete Employee");
+            Console.WriteLine("4. Get Employee By ID");
+            Console.WriteLine("5. List All Employees");
+            Console.WriteLine("6. Sort Employees");
+            Console.WriteLine("7. Filter Employees");
+            Console.WriteLine("8. Group Employees");
+            Console.WriteLine("9. Exit");
 
-Console.WriteLine("Hello, World!");
+            Console.WriteLine("Choose Option : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 9)
+            {
+                break;
+            }
+        }
+    }
+}
